b3fbfa16f39e791fae13b3e3a756c6dc624c70b6
OG-Fishies <165400445+EggRecRoom@users.noreply.github.com>
2025-03-10 18:12:26 +0000

    .

---
+++ main.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, jsonify, send_file, redirect, make_response, url_for, render_template, Response, abort, session
+from flask_sock import Sock
+from flask_cors import CORS
+import asyncio
+import functools
+import json
+import requests
+import random
+import colorama
+from colorama import Fore
+import os
+import base64
+import sys
+from enum import Enum
+import jwt
+from dotenv import load_dotenv
+load_dotenv()
+
+name = f"{__name__}.py"
+
+debug = os.getenv("DEBUG")
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+sock = Sock(app)
+CORS(app)
+
+@app.errorhandler(500)
+def q405(e):
+    data = {"Message":"An error has occurred."}
+    return jsonify(data), 500
+
+def run():
+    Port = 9020
+    Ip = "0.0.0.0"
+    app.run(str(Ip), int(Port), debug, ssl_context='adhoc')
+
+run()
